c1861ab59d2cd48125ea35c38ddd06b05195f622
Pooja pokharel <pooja.pokhrel36@gmail.com>
2021-02-03 06:06:17 +0545

    checkout form component step

---
+++ src/components/CheckoutForm.js
@@ -0,0 +1,14 @@
+import { useEffect, useState } from 'react';
+
+const CheckoutForm = ({ totalPrice, paymentHandler }) => {
+	
+	useEffect(() => {
+		const createPaymentIntent = async () => {
+			
+		};
+
+		createPaymentIntent();
+	}, []);
+};
+
+export default CheckoutForm;
